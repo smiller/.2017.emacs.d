e898da93a0d2806e032c58d46d6c113f89a3771a
Sean Miller <sean@seanmiller.ca>
2017-03-13 01:39:25 -0400

    Add org2blog code
    Note that wp-post-buffer seems to fail if there are any
    smart characters (quotes, ellipses, en-dashes) in the
    buffer to be posted.  Haven't figured out why.

--- init.el
+++ init.el
@@ -26,7 +26,7 @@
  '(custom-enabled-themes (quote (dichromacy)))
  '(package-selected-packages
    (quote
-    (projectile-rails rake f s inf-ruby inflections helm-projectile helm helm-core popup typo rvm rubocop enh-ruby-mode org-beautify-theme org-bullets hydra pkg-info epl magit magit-popup git-commit with-editor dash async projectile use-package))))
+    (org2blog metaweblog xml-rpc projectile-rails rake f s inf-ruby inflections helm-projectile helm helm-core popup typo rvm rubocop enh-ruby-mode org-beautify-theme org-bullets hydra pkg-info epl magit magit-popup git-commit with-editor dash async projectile use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -208,3 +208,60 @@
 ;; http://www.virtuouscode.com/2010/04/23/daemonic-emacs/
 ;; http://www.virtuouscode.com/2011/10/27/running-emacs-as-a-server-emacs-reboot-15/
 (server-start)
+
+;; org2wp
+
+;; Taking the org2blog instructions from http://vxlabs.com/2014/05/25/emacs-24-with-prelude-org2blog-and-wordpress/
+
+;; but following the org2blog README and using auth-source instead of netrc: https://github.com/punchagan/org2blog/commit/52be89507c337e5f74be831ca563a8023e0ec736
+
+;; Expects an ~/.authinfo file of the form: machine {my-machine-name} login {my-elogin} password {my-password}
+
+ (setq org-directory "~/Dropbox/gesta/twc/")
+ ;; and you need this, else you'll get symbol void errors when doing
+ ;; fill paragraph
+ (setq org-list-allow-alphabetical t)
+
+(quelpa 'org2blog)
+(quelpa 'auth-source)
+(quelpa 'htmlize)
+
+ (require 'org2blog-autoloads)
+ (require 'auth-source)
+ (let (credentials)
+	    (add-to-list 'auth-sources "~/.authinfo")
+	    (setq credentials (auth-source-user-and-password "thewanderingcoder"))
+	    (setq org2blog/wp-blog-alist
+		  `(("twc"
+		     :url "http://thewanderingcoder.com/xmlrpc.php"
+		     :username ,(car credentials)
+		     :password ,(cadr credentials)
+		     :default-title "Hello World"
+		     :default-categories ("org2blog" "emacs")
+		     :tags-as-categories nil))))
+
+ ;; http://blog.binchen.org/posts/how-to-use-org2blog-effectively-as-a-programmer.html
+ ;; has half the instructions, but was missing
+ ;; `wp-use-sourcecode-shortcode` at the time of this writing, without
+ ;; which this does not work at all.
+
+ ;; * `M-x package-install RET htmlize` is required, else you get empty
+ ;;   code blocks https://github.com/punchagan/org2blog/blob/master/org2blog.el
+ ;; * with wp-use-sourcecode-shortcode set to 't, org2blog will use 1
+ ;;   shortcodes, and hence the SyntaxHighlighter Evolved plugin on your blog.
+ ;;   however, if you set this to nil, native Emacs highlighting will be used,
+ ;;   implemented as HTML styling. Your pick!
+ (setq org2blog/wp-use-sourcecode-shortcode 't)
+ ;; removed light="true"
+ (setq org2blog/wp-sourcecode-default-params nil)
+ ;; target language needs to be in here
+ (setq org2blog/wp-sourcecode-langs
+	  '("actionscript3" "bash" "coldfusion" "cpp" "csharp" "css" "delphi"
+	    "erlang" "fsharp" "diff" "groovy" "javascript" "java" "javafx" "matlab"
+	    "objc" "perl" "php" "text" "powershell" "python" "ruby" "scala" "sql"
+	    "vb" "xml"
+	    "sh" "emacs-lisp" "lisp" "lua"))
+
+ ;; this will use emacs syntax higlighting in your #+BEGIN_SRC
+ ;; <language> <your-code> #+END_SRC code blocks.
+ (setq org-src-fontify-natively t)
